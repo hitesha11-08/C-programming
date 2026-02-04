9f65a83f9eb242a54fdfbeba9524394ef62a93a7
hitesha11-08 <hiteshamurugan112004@gmail.com>
2026-02-04 22:05:30 +0530

    Update CountVowelsConsonants.c

--- C Programs/CountVowelsConsonants.c
+++ C Programs/CountVowelsConsonants.c
@@ -1,9 +1,24 @@
 #include<stdio.h>
-int main()
-{
-int a[5]={1,2,3,4,5};
-printf("size of a:%lu\n",sizeof(a));
-printf(" size of a[0]:%lu\n",sizeof(a[0]));
+int main(){
+char s[100];
+int i,v=0,c=0,d=0,sp=0;
+printf("Enter string:");
+fgets(s,100,stdin);
+for(i=0;s[i]!='\0';i++){
+if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||
+   s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
+v++;
+else if(s[i]>='0'&&s[i]<='9')
+d++;
+else if(s[i]==' ')
+sp++;
+else if((s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z'))
+c++;
+}
+printf("Vowels=%d\n",v);
+printf("Consonants=%d\n",c);
+printf("Digits=%d\n",d);
+printf("Spaces=%d",sp);
 return 0;
 }
 
