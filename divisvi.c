9279e76dce0939b49c809faacae4b39168df5354
hitesha11-08 <hiteshamurugan112004@gmail.com>
2026-02-04 21:03:24 +0530

    Add files via upload

---
+++ divisvi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() 
+    {
+        int a;
+    for (int i=3;i<=15;i++)
+    {
+        if((i%3==0)||(i%5==0))
+        {
+            printf("%d",i);
+        }
+    }
+   return 0;
+      }
+
+
