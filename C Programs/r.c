554bbf00e9324b5d4176ad510d3ec5023107635a
Hitesha M <hiteshamurugan112004@gmail.com>
2026-03-18 16:35:58 +0530

    Add files via upload

---
+++ C Programs/r.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n, count = 0;
+    scanf("%d", &n);
+    while(n) {
+        count += n & 1;
+
+n>>=1;
+    }
+ printf("Set bits = %d", count);
+    return 0;
+}
+
